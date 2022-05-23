98e178dde0db22d53e4c23d49016437505915cce
DuyLe <minhduy4520@gmail.com>
2022-05-23 00:01:43 -0500

    Update InsertInterval.java

--- Google/InsertInterval.java
+++ Google/InsertInterval.java
@@ -16,16 +16,16 @@ public class InsertInterval{
         int[][] intervals={{1,3}, {6,9}};
         int[] newInterval={2,5};
         ArrayList<Interval> result=new ArrayList<Interval>();
-        Interval temp;
 
         if(newInterval[1]<intervals[0][0]){
-            temp.start=newInterval[0];
-            temp.end=newInterval[1];
-            result.add(temp);
-            for(int i=0;i<intervals.length; i++) result.temp(new Interval());
+            result.add(new Interval(newInterval[0], newInterval[1]));
+            for(int i=0;i<intervals.length; i++) result.add(new Interval(intervals[i][0], intervals[i][1]));
         else if(newInterval[0]>intervals[intervals.length][1]]){
-            for(int i=0;i<intervals.length; i++) result.put(intervals[i][0], intervals[i][1]);
-            result.put(newInterval[0], newInterval[1]);
+            for(int i=0;i<intervals.length; i++) result.add(new Interval(intervals[i][0], intervals[i][1]));
+            result.add(new Interval(newInterval[0], newInterval[1]));
+        }
+        else{
+
         }
 
         for(int i=0;i<intervals.length; i++) result.put(intervals[i][0], intervals[i][1]);
@@ -33,9 +33,6 @@ public class InsertInterval{
 
         int[][] finals=new int[result.size()][2];
         int i;
-        for(Map.Entry<Integer, Integer> e: result.entrySet()){
-            finals[i][0]=e.getKey();
-            finals[i][1]=e.getValue();
-        }
+       
     }
 }
