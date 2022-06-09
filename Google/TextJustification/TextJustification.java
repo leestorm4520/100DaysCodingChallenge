c7532ae65bfc613767fc846614f27351447e4f01
DuyLe <minhduy4520@gmail.com>
2022-06-08 23:43:14 -0500

    Update TextJustification.java

--- Google/TextJustification/TextJustification.java
+++ Google/TextJustification/TextJustification.java
@@ -70,24 +70,34 @@ Algorithm:
 
 import java.util.*;
 public class TextJustification {
-  //find the right index
+  //find the right index by adding the next word length until it exceeds the maxWidth
     private static int findRight(int left, String[] words, int maxWidth){
       int right=left;
-      return right;
+      int sum=words[right++].length();
+
+      while(right<words.length && (sum+1+words[right].length())<=maxWidth){
+        sum+=1+words[right++].length();
+      }
+
+      return right-1;
     }
     private static String justify(int left, int right, String[] words, int maxWidth){
 
     }
     private static int wordsLength(int left, int right, String[] words){
-      int wordLength=0;
-      return wordLength;
+      int wordsLength=0;
+      for(int i=0; i<=right;i++) wordsLength+=words[i].length();
+      return wordsLength;
 
     }
     private static String padResult(String result, int maxWidth){
-      
+      return result+blank(maxWidth-result.length());
     }
     private static String blank(int length){
-
+      String blank="";
+      for(int i=0;i<length;i++) blank+=" ";
+      return blank;
+      //return new String(new char[length]).replace('\0',' ');
     }
     public static void main(String args[]){
       String[] words=new String[]{"This", "is", "an", "example", "of", "text", "justification."};
