cd9efb2f6872f090d921010a113a143cef348da5
DuyLe <minhduy4520@gmail.com>
2022-06-18 09:57:38 -0500

    update

--- Miscellaneous/Sliding Window/slidingImplement.java
+++ Miscellaneous/Sliding Window/slidingImplement.java
@@ -6,5 +6,25 @@
  */
 
 public class slidingImplement {
+    static int maxSum(int arr[], int n, int k){
+        if(n<k){
+            System.out.println("Invalid");
+            return -1;
+        }
+        int maxSum=0;
+        for(int i=0; i<k;i++) maxSum+=arr[i];
+        int windowSum= maxSum;
+        for(int i=k; i<k;i++){
+            windowSum+=arr[i]-arr[i-k];
+            maxSum=Math.max(maxSum, windowSum);
+        }
+        return maxSum;
+    }
+    public static void main(String[] args){
+        int arr[]={1,4,2,10,2,3,1,0,20};
+        int k=4;
+        int n=arr.length;
+        System.out.println(maxSum(arr, n, k));
+    }
     
 }
