6d8926a859a4804942ae2723aed0be45ae9f533b
DuyLe <minhduy4520@gmail.com>
2022-06-18 00:47:22 -0500

    Update slidingImplement.java

--- Miscellaneous/Sliding Window/slidingImplement.java
+++ Miscellaneous/Sliding Window/slidingImplement.java
@@ -1,3 +1,10 @@
+/*
+ * Window Sliding Technique is a computational technique to reduce the use of tested loop
+ * and replace with single loop, reducing T(N)
+ * 
+ * size of window is fixed
+ */
+
 public class slidingImplement {
     
 }
