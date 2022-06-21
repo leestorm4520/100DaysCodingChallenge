9eb1bbc983675f79bb58fff589d92c7ced4a837d
DuyLe <minhduy4520@gmail.com>
2022-06-21 14:15:09 -0500

    update

--- Google/MinWindowSubstring/MinWindow.java
+++ Google/MinWindowSubstring/MinWindow.java
@@ -55,7 +55,7 @@ public class MinWindow {
     public static String minWindow(String s, String t){
         //create a dictionary using array
         int[] arr_char=new int[128];
-        for(char c: s.toCharArray()) arr_char[c]++; //character and its corresponding frequency in the string s
+        for(char c: t.toCharArray()) arr_char[c]++; //character and its corresponding frequency in the string s
 
         int left=0, right=0, minStart=0, counter= t.length(), minLength=Integer.MAX_VALUE;
         while(right<s.length()){
